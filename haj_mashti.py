444d9ef52153f62716a1e42afb14fe7c493d58f3
Erfan Rostami <150842828+erfanrostami0@users.noreply.github.com>
2024-04-25 00:22:33 +0330

    Add files via upload

---
+++ haj_mashti.py
@@ -0,0 +1,14 @@
+tajrobe=input()
+haj=tajrobe[0]
+karbala=tajrobe[1]
+mashhad=tajrobe[2]
+if haj=='Y':
+    print('Haji')
+elif karbala=='Y':
+    print('Karbalaee')
+elif mashhad=='Y':
+    print('Mashti')
+else:
+    print('Agha')
+
+
